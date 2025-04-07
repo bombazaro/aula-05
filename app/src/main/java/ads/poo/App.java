3a3ca35a82c87229e4bff50c618d2a3727f24ddd
Bombazaro <bernardo.b14@aluno.ifsc.edu.br>
2025-04-07 16:21:57 -0300

    Concluído amém

--- app/src/main/java/ads/poo/App.java
+++ app/src/main/java/ads/poo/App.java
@@ -3,39 +3,28 @@ package ads.poo;
 import java.util.Scanner;
 
 public class App {
-
-    //public static void main(String[] args) {
-    //int[][] matriz = new int[3][3];
-
-    //matriz[0][0] = 1;
-    // TODO usar o tamanho da matriz e não estático
-    //for (int i = 0; i < 3; i++) {
-    //for (int j = 0; j < 3; j++) {
-    //  System.out.printf(matriz[i][j] + " ");
-    // }
-    // System.out.println();
-    //  }
-    //}
-//}
-
-//matriz quadrada min 3 max 10. gerar a matriz identidade na tela
-
+    // Cria uma matriz identidade de tamanho n1 x n1, onde n1 é um número entre 3 e 10
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         int n1;
 
         do {
-            System.out.print("Insira uma número entre 3 e 10: ");
+            System.out.print("Insira um número entre 3 e 10 para a matriz identidade: ");
             n1 = entrada.nextInt();
+
         }while(n1<3 || n1>10);
 
         int[][] matriz = new int[n1][n1];
 
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
+            for (int i = 0; i < n1; i++) {
+            for (int j = 0; j < n1; j++) {
+                if (i == j) {
+                    matriz[i][j] = 1;
+                }
                 System.out.printf(matriz[i][j] + " ");
             }
+            System.out.println();
         }
+        entrada.close();
     }
-
 }
